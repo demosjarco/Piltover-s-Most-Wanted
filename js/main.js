4ff80f12f37af70656ee2f97be3a5c6e7fed5ad8
Victor <vilisei@gmail.com>
2016-05-02 14:17:04 -0700

    Added going through match list

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ firebaseRef.child("apiControl/apiKey").once("value", function(key) {
 				counter1++;
 				
 				if (!summoner["recentGamesLoaded"]) {
-					console.log(summoner);
-					
 					$.ajax({
 						type: 'GET',
 						url: 'https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/' + summoner["summonerId"] + '?beginTime=1461351600000&endTime=1461610800000&api_key=' + key.val(),
@@ -37,8 +35,6 @@ firebaseRef.child("apiControl/apiKey").once("value", function(key) {
 						},
 						dataType: 'json',
 						success: function(data) {
-							console.log(data);
-							
 							if (data["totalGames"] > 0) {
 								var matches = data["matches"];
 								
@@ -46,7 +42,7 @@ firebaseRef.child("apiControl/apiKey").once("value", function(key) {
 									for (var x = 0; x < matches.length; x++) {
 										var matchIdValue = matches[x]["matchId"];
 										
-										if (!snapshot.child('' + matchIdValue).exists()) {
+										if (!snapshot.child(matchIdValue).exists()) {
 											var temp = {};
 											temp[matchIdValue] = {summonersLoaded: false, bannedChampionsLoaded: false, matchId: matchIdValue};
 											firebaseRef.child("matchIds").update(temp);
@@ -93,6 +89,114 @@ firebaseRef.child("apiControl/apiKey").once("value", function(key) {
 	goThroughSummonerList();
 	
 	function goThroughMatchList() {
+		firebaseRef.child("matchIds").once("value", function(matchIds) {
+			var matchList = matchIds.val();
+			
+			var addedNewSummoners = false;
+			var addedNewBannedChamps = false;
+			var counter1 = 0;
+			function manualForLoop1(arr) {
+				var matchGame = matchList[arr[counter1]];
+				counter1++;
+				
+				if (!matchGame["summonersLoaded"]) {
+					$.ajax({
+						type: 'GET',
+						url: 'https://na.api.pvp.net/api/lol/na/v2.2/match/' + matchGame["matchId"] + '?includeTimeline=false&api_key=' + key.val(),
+						async: true,
+						error: function(errorContent) {
+							// Just skip
+							if (counter1 < Object.keys(matchList).length) {
+								manualForLoop1(Object.keys(matchList));
+							} else {
+								if (addedNewSummoners) {
+									// Go get the new summoners
+									goThroughSummonerList();
+								}
+							}
+						},
+						dataType: 'json',
+						success: function(data) {
+							firebaseRef.child("summonerIds").once("value", function(snapshot) {
+								var participantIdentities = data["participantIdentities"];
+								
+								for (var x = 0; x < participantIdentities.length; x++) {
+									var summonerId = participantIdentities[x]["player"]["summonerId"];
+									
+									if (!snapshot.child(summonerId).exists()) {
+										var temp = {};
+										temp[summonerId] = {recentGamesLoaded: false, summonerId: summonerId};
+										
+										firebaseRef.child("summonerIds").update(temp);
+										
+										addedNewSummoners = true;
+									}
+								}
+							});
+							
+							// Marked as summonersLoaded
+							firebaseRef.child("matchIds/" + matchGame["matchId"]).update({
+								summonersLoaded: true
+							});
+							
+							firebaseRef.child("bannedChamps").once("value", function (snapshot) {
+								var teams = data["teams"];
+								for (var t = 0; t < teams.length; t++) {
+									var bans = teams[t]["bans"];
+									
+									for (var b = 0; b < bans.length; b++) {
+										var bannedChampId = bans[b]["championId"];
+										
+										if (snapshot.child(bannedChampId).exists()) {
+											// Increment ban count
+											var oldBanCount = snapshot.child(bannedChampId + "/timesBanned").val();
+											firebaseRef.child("bannedChamps/" + bannedChampId).update({
+												timesBanned: oldBanCount + 1
+											});
+										} else {
+											// New ban champ
+											var temp = {};
+											temp[bannedChampId] = {champId: bannedChampId, timesBanned: 1};
+											
+											firebaseRef.child("bannedChamps").update(temp);
+											
+											addedNewBannedChamps = true;
+										}
+									}
+								}
+							});
+							
+							// Marked as bannedChampionsLoaded
+							firebaseRef.child("matchIds/" + matchGame["matchId"]).update({
+								bannedChampionsLoaded: true
+							});
+							
+							// Step through summoner list
+							if (counter1 < Object.keys(matchList).length) {
+								manualForLoop1(Object.keys(matchList));
+							} else {
+								if (addedNewSummoners) {
+									// Go get the new summoners
+									goThroughSummonerList();
+								}
+							}
+						},
+					});
+				} else {
+					// Step through match list
+					if (counter1 < Object.keys(matchList).length) {
+						manualForLoop1(Object.keys(matchList));
+					} else {
+						if (addedNewSummoners) {
+							// Go get the new summoners
+							goThroughSummonerList();
+						}
+					}
+				}
+			}
+			// Run first time
+			manualForLoop1(Object.keys(matchList));
+		});
 	}
 	// Run first time
 	goThroughMatchList();
