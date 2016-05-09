e7bebb51a04134a132c1094ae8ee11a1030ff070
Victor <vilisei@gmail.com>
2016-05-08 18:19:34 -0700

    Adding grid item for each champion

--- js/presentation.js
+++ js/presentation.js
@@ -1,22 +1,70 @@
 // Presenting the content
+var firebaseRef = new Firebase("https://pilt-most-want.firebaseio.com/");
+
 function loadUI() {
 	// Disable until finished loading
 	$("#numberOfBannedChampsToLoad").prop("disabled", true);
 	clearGrid();
 	
-	var firebaseRef = new Firebase("https://pilt-most-want.firebaseio.com/");
-	
+	// Seems redundant but it wont work otherwise...sigh -_-
+	var numberToLoad = 0;
+	for (var i = 0; i < $("#numberOfBannedChampsToLoad").val(); i++) {
+		numberToLoad++;
+	}
 	var count = 0;
 	
-	firebaseRef.child("bannedChamps").orderByChild("timesBanned").on("child_added", function(snapshot) {
-		count++;
-		$("#numberOfBannedChampsToLoad").attr({"max": count});
+	firebaseRef.child("matchIds").once("value", function(snapshot3) {
+		var numberOfGames = Object.keys(snapshot3.val()).length;
 		
-		// Enable it now that it's finished loading everything
 		firebaseRef.child("bannedChamps").once("value", function(snapshot2) {
-			if (count >= Object.keys(snapshot2.val()).length) {
-				$("#numberOfBannedChampsToLoad").prop("disabled", false);
-			}
+			$("#numberOfBannedChampsToLoad").attr({"max": Object.keys(snapshot2.val()).length});
+			firebaseRef.child("bannedChamps").orderByChild("timesBanned").limitToLast(numberToLoad).on("child_added", function(snapshot) {
+			// firebaseRef.child("bannedChamps").limitToFirst(numberToLoad).on("child_added", function(snapshot) {
+				count++;
+				var percentage = (snapshot.val()["timesBanned"] / numberOfGames);
+				var nicePercentage = Math.round(percentage * 1000) / 10;
+				
+				var averageChampLevel = 1;
+				var averageChampPoints = snapshot.val()["championPoints"] / snapshot.val()["summonersForAverage"];
+				console.log(averageChampPoints);
+				var outOfChampPoints = 0;
+				if (averageChampPoints < 1800) {
+					// Level 1
+					averageChampLevel = 1;
+					outOfChampPoints = 1800;
+				} else if (averageChampPoints >= 1800 && averageChampPoints < 6000) {
+					// Level 2
+					averageChampLevel = 2;
+					outOfChampPoints = 6000;
+				} else if (averageChampPoints >= 6000 && averageChampPoints < 12600) {
+					// Level 3
+					averageChampLevel = 3;
+					outOfChampPoints = 12600;
+				} else if (averageChampPoints >= 12600 && averageChampPoints < 21600) {
+					// Level 4
+					averageChampLevel = 4;
+					outOfChampPoints = 21600;
+				} else if (averageChampPoints >= 21600) {
+					// Level 5
+					averageChampLevel = 5;
+					outOfChampPoints = averageChampPoints;
+				}
+				
+				$("#grid").append('<div class="item"><div class="champImage ' + snapshot.val()["champId"] + '"></div><span class="banPercent">' + nicePercentage + '%</span><span class="champLevel">Level ' + averageChampLevel + '</span></div>');
+				$(".champImage." + snapshot.val()["champId"]).circleProgress({
+					value: averageChampPoints / outOfChampPoints,
+					size: 105,
+					fill: {
+						gradient: ["#8F7514", "#C9A932"]
+					}
+				});
+				setTimeout(function(){loadChampImage(snapshot.val()["champId"])}, 0);
+				
+				// Enable it now that it's finished loading everything
+				if (count == $("#numberOfBannedChampsToLoad").val()) {
+					$("#numberOfBannedChampsToLoad").prop("disabled", false);
+				}
+			});
 		});
 	});
 }
@@ -24,4 +72,22 @@ function loadUI() {
 loadUI();
 
 function clearGrid() {
+	$("#grid").empty();
+}
+
+function loadChampImage(champId) {
+	firebaseRef.child("apiControl").once("value", function(snapshot) {
+		$.ajax({
+			type: 'GET',
+			url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + champId + '?champData=image&api_key=' + snapshot.val()["apiKey"],
+			async: true,
+			error: function(errorContent) {
+				console.log(errorContent);
+			},
+			dataType: 'json',
+			success: function(data) {
+				$(".champImage." + champId).css("background-image", "url(https://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/" + data["image"]["full"] + ")");
+			},
+		});
+	});
 }
